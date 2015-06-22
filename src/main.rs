987e7c7e24da93dc52439fe3110b512519fc19c5
Michael Nitschinger <michael@nitschinger.at>
2015-06-22 14:37:27 +0200

    small refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod net;
 use net::ping::Ping;
 
 fn main() {
+	// todo: allow to pass in hostname
+	// print when it starts pinging and in green OK of in red FAILURE
+	// if green keep going, if failure print the returned failure nicely.
     let ping = Ping::new(String::from("127.0.0.1"));
 
     println!("{:?}", ping.run());
--- src/net/ping.rs
+++ src/net/ping.rs
@@ -1,6 +1,40 @@
 use std::process::Command;
 
 #[derive(Debug)]
+pub struct PingResponse {
+	destination: String,
+	packets: Vec<PingPacket>
+}
+
+#[derive(Debug)]
+pub struct PingPacket {
+	icmp_seq: i32,
+	time: f64,
+	ttl: i32
+}
+
+impl PingPacket {
+
+	fn from(source: &str) -> Vec<PingPacket> {
+		source
+			.lines()
+			.filter(|line| String::from(*line).contains("time="))
+			.map(|line| line.split(' ').collect::<Vec<&str>>())
+			.map(|words| {
+				let icmp_seq = words[4].split('=').collect::<Vec<&str>>();
+				let ttl = words[5].split('=').collect::<Vec<&str>>();
+				let time = words[6].split('=').collect::<Vec<&str>>();
+				PingPacket { 
+					icmp_seq: icmp_seq[1].parse().unwrap(), 
+					time: time[1].parse().unwrap(), 
+					ttl: ttl[1].parse().unwrap() 
+				}
+			})
+			.collect::<Vec<PingPacket>>()
+	}
+}
+
+#[derive(Debug)]
 pub struct Ping {
 	destination: String
 }
@@ -12,41 +46,28 @@ impl Ping {
 	}
 
 	pub fn run(&self) -> PingResponse {
-		let output = Command::new("ping")
+		let executed = Command::new("ping")
 			.arg("-c 1")
 			.arg(self.destination.clone())
 			.output()
 			.unwrap_or_else(|e| { panic!("failed to execute process: {}", e) });
 
+		// Todo: return result type with errors
+		// Check status and if != 0 translate into error status
+		// do not panic if error in this module, just raise the error to the caller (remove unwraps)
+		// add support for custom -c invocations
+		// error if ping does not exist
+		// error if hostname not found or other error
+		// test it????
+		// write docs to see how it is generated
 
-		let output = String::from_utf8_lossy(&output.stdout);
-
-		let packets: Vec<PingPacket> = output
-			.lines()
-			.filter(|line| String::from(*line).starts_with("64"))
-			.map(|line| {
-				let split = line.split(' ').collect::<Vec<&str>>();
-				let icmp_seq = split[4].split('=').collect::<Vec<&str>>();
-				let ttl = split[5].split('=').collect::<Vec<&str>>();
-				let time = split[6].split('=').collect::<Vec<&str>>();
-				PingPacket { icmp_seq: icmp_seq[1].parse().unwrap(), time: time[1].parse().unwrap(), ttl: ttl[1].parse().unwrap() }
-			})
-			.collect();
+		let stdout = String::from_utf8_lossy(&executed.stdout);
+		let packets = PingPacket::from(&*stdout);
 		
-		PingResponse { destination: self.destination.clone(), packets: packets }
+		PingResponse { 
+			destination: self.destination.clone(), 
+			packets: packets 
+		}
 	}
 
-}
-
-#[derive(Debug)]
-struct PingResponse {
-	destination: String,
-	packets: Vec<PingPacket>
-}
-
-#[derive(Debug)]
-struct PingPacket {
-	icmp_seq: i32,
-	time: f64,
-	ttl: i32
 }
